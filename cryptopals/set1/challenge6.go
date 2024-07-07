398b1c410c5031b0733a0f85625890f43491b178
Cristian <cristian.anghel4@gmail.com>
2024-07-07 17:12:56 +0300

    complete challenge 6 my way;

--- cryptopals/set1/challenge6.go
+++ cryptopals/set1/challenge6.go
@@ -39,7 +39,29 @@ func Challenge6() {
 	const KEYSIZE = 40
 	hammings := make([]data, 0)
 	for keysize := 2; keysize <= KEYSIZE; keysize++ {
-		normalizedDistance := averageHammingDistance(decoded, keysize)
+		interm := make([]data, 0)
+		for i := 0; (i+2)*keysize < len(decoded); i++ {
+			b1 := decoded[i*keysize : (i+1)*keysize]
+			b2 := decoded[(i+1)*keysize : (i+2)*keysize]
+			dst, err := hammingDistance(b1, b2)
+			if err != nil {
+				panic(err)
+			}
+
+			normalized := float32(dst) / float32(keysize)
+			interm = append(interm, data{
+				keysize:      keysize,
+				hammingScore: normalized,
+			})
+		}
+
+		total := float32(0)
+		for _, inter := range interm {
+			total += inter.hammingScore
+		}
+
+		normalizedDistance := total / float32(len(interm))
+
 		hammings = append(hammings, data{
 			keysize:      keysize,
 			hammingScore: normalizedDistance,
@@ -74,25 +96,6 @@ func Challenge6() {
 	decrypted := xorRepeatingCycle(decoded, guessedKey)
 	fmt.Printf("guessed output: %s\n", decrypted)
 	fmt.Printf("guessed key: %s\n", guessedKey)
-}
-
-func averageHammingDistance(decoded []byte, keysize int) float32 {
-	var distances []float64
-	for i := 0; i+2*keysize <= len(decoded); i += keysize {
-		d1 := decoded[i : i+keysize]
-		d2 := decoded[i+keysize : i+(2*keysize)]
-		distance, err := hammingDistance(d1, d2)
-		if err != nil {
-			panic(err)
-		}
-		normalized := float64(distance) / float64(keysize)
-		distances = append(distances, normalized)
-	}
-	var total float64
-	for _, d := range distances {
-		total += d
-	}
-	return float32(total / float64(len(distances)))
 }
 
 func makeBlocks(cipher []byte, keysize int) [][]byte {
